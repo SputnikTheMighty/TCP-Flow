684623b3cbfc3e314e1f23294f14eb7e80565859
SputnikTheMighty <r-richardson@hotmail.co.uk>
2023-03-06 21:51:31 +0100

    not dropping messages, but very slow

--- message.py
+++ message.py
@@ -1,48 +1,69 @@
 import struct
+import time
 from collections import namedtuple
+
+class MessageException(Exception):
+    pass
 
 class Message:
 
-    SOF = 0x7E
-    ESC = 0x7D
-    XOR = 0x20
+    SOF = b'\x7E'
+    EOF = b'\x7C'
+    ESC = b'\x7D'
+    XOR = b'\x20'
 
     Header = namedtuple("Header", "sof len cmd")
 
-    def __init__(self) -> None:
+    TIMEOUT_MS = 5000
+
+    def __init__(self, tcp_conn) -> None:
         self.message = bytearray()
         self.last_byte_was_escape = False
         self.new_messages = 0
+        self.conn = tcp_conn
 
     def validate_message(self) -> bytearray:
         header = self.Header(*struct.unpack('>BHB', self.message[:struct.calcsize('>BHB')]))
-        assert header.sof == self.SOF, "SOF not present"
-        assert header.len == len(self.message) - len(header), "length of message is {}".format(len(self.message))
+        assert header.sof == int.from_bytes(self.SOF, 'big'), "SOF not present"
+        assert header.len == len(self.message) - len(header) - 1, "length of message is {}".format(len(self.message))
+        assert self.message[-1] == int.from_bytes(self.EOF, 'big'), "EOF not present"
         self.data = self.message[struct.calcsize('>BHB'):]
         return header.cmd
 
-    def append(self, raw):
-        for b in raw:
+    def get_message(self):
+
+        tick = time.ticks_ms()
+        while time.ticks_ms() - tick < self.TIMEOUT_MS:
+
+            b = self.conn.recv(1)
+            if b is None:
+                continue
 
             if self.last_byte_was_escape:
                 self.last_byte_was_escape = False
                 
                 if b == self.SOF:
-                    print("data transfer cancelled by sender")
                     self.message = bytearray()
+                    raise MessageException("data transfer cancelled")
                 else:
-                    self.message.append(b ^ self.XOR)
+                    self.message += (b ^ self.XOR)
             
             elif b == self.SOF:
                 self.message = bytearray()
-                self.message.append(b)
+                self.message += b
                 self.new_messages += 1
+
+            elif b == self.EOF:
+                self.message += b
+                return
             
             elif b == self.ESC:
                 self.last_byte_was_escape = True
 
             else:
-                self.message.append(b)
+                self.message += b
+
+        raise MessageException("Get message timeout")
 
     @classmethod
     def construct(cls, data):
@@ -50,7 +71,7 @@ class Message:
         input += data
 
         message = bytearray()
-        message.append(cls.SOF)
+        message += cls.SOF
 
         for i in input:
             if i == cls.SOF or i == cls.ESC:
@@ -59,6 +80,7 @@ class Message:
             else:
                 message.append(i)
 
+        message += cls.EOF
         return message
     
 
--- upy_firmware.py
+++ upy_firmware.py
@@ -33,37 +33,43 @@ while True:
     conn, addr = server.accept()
     print('Connected by', addr)
 
-    message = Message()
+    message = Message(conn)
     completed_messages = 0
+    total_write_time = 0
+    total_read_time = 0
 
     try:
 
         while True:
 
             try:
-                # Receive data from the client
-                data = conn.recv(1024)
-                message.append(data)
+                tick_read = time.ticks_us()
+                message.get_message()
                 command = message.validate_message()
                 completed_messages += 1
+                tock_read = time.ticks_us()
+                total_read_time += (tock_read - tick_read)
 
                 values = message.data
 
+                tick_write = time.ticks_us()
                 while len(values) > 3:
                     if values[0] < NUM_LEDS:
                         np[values[0]] = (values[1], values[2], values[3])
                     values = values[4:]
-
-                # Update the LEDs
                 np.write()
+                tock_write = time.ticks_us()
+                total_write_time += (tock_write - tick_write)
             
             except KeyboardInterrupt:
                 raise
            
             except Exception as e:
-                print(e)
+                print("exception in main: {}".format(e))
 
     finally:
         print("messages: {}".format(message.new_messages))
         print("completed messages: {}".format(completed_messages))
+        print("average write time: {}".format(total_write_time/completed_messages))
+        print("average read time: {}".format(total_read_time/completed_messages))
         conn.close()
